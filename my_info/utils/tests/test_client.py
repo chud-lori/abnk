a1760d1a17a65c055ee8841b972fdeaadd30ae99
chud-lori <imlori000@gmail.com>
2025-04-30 12:08:06 +0700

    test: fix client test

--- my_info/utils/tests/test_client.py
+++ my_info/utils/tests/test_client.py
@@ -1,16 +1,21 @@
 import unittest
-
+from django.utils.crypto import get_random_string
 from my_info.utils.client import MyInfoPersonalClientV4
+from my_info.utils.security import generate_code_challenge
 
 
 class TestMyInfoPersonalClientV4(unittest.TestCase):
     maxDiff = None
 
     def test_get_authorise_url(self):
+        oauth_state = get_random_string(length=16)
+        callback_url = "http://localhost:3001/callback"
+        code_chal = generate_code_challenge(oauth_state)
+
         authorise_url = MyInfoPersonalClientV4().get_authorise_url(
-            "abc123", "https://backend.local.abnk.ai/myinfo/callback"
+            oauth_state, callback_url
         )
         self.assertEqual(
             authorise_url,
-            "https://test.api.myinfo.gov.sg/com/v4/authorize?client_id=STG-202327956K-ABNK-BNPLAPPLN&scope=uinfin%20name%20sex%20race%20dob%20residentialstatus%20nationality%20birthcountry%20passtype%20passstatus%20passexpirydate%20employmentsector%20mobileno%20email%20regadd%20housingtype%20hdbtype%20cpfcontributions%20noahistory%20ownerprivate%20employment%20occupation%20cpfemployers%20marital&purpose_id=7ed6f2ce&response_type=code&code_challenge=bKE9UspwyIPg8LsQHkJaiehiTeUdstI5JZOvaoQRgJA&code_challenge_method=S256&redirect_uri=https://backend.local.abnk.ai/myinfo/callback",  # noqa: E501
+            f"https://test.api.myinfo.gov.sg/com/v4/authorize?client_id=STG2-MYINFO-SELF-TEST&scope=uinfin%20name%20sex%20race%20nationality%20dob%20email%20mobileno%20regadd%20housingtype%20hdbtype%20marital%20edulevel%20noa-basic%20ownerprivate%20cpfcontributions%20cpfbalances&purpose_id=None&response_type=code&code_challenge={code_chal}&code_challenge_method=S256&redirect_uri=http://localhost:3001/callback",  # noqa: E501
         )
